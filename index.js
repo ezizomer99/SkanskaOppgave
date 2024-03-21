21d22db1d2ef646f0ca20ce7c28c582af3cc3328
Eziz <ezizomer1999@gmail.com>
2024-03-21 22:04:04 +0100

    mer endringer

--- index.js
+++ index.js
@@ -11,5 +11,23 @@ for (let year = 1901; year <= 2000; year++) {
     }
 }
 
+
 document.getElementById("result").innerHTML = `Antall ganger der søndag var den første dagen i måneden fra perioden 1901-2000: ${amountOfSunday}`;
 });
+
+/*
+function findSundays() {
+    let sundays = [];
+
+    for (let year = 1901; year <= 2000; year++) {
+        for (let month = 0; month < 12; month++) {
+            let date = new Date(year, month, 1);
+            if (date.getDay() === 0) {
+                sundays.push(date.toISOString().substring(0, 10));
+            }
+        }
+    }
+    return sundays;
+}
+console.log(findSundays());
+*/
